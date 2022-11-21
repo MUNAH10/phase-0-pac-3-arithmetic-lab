260ea050528f07e6a5d5593465313d13537a5deb
MUNAH10 <munahassan5075@gmail.com>
2022-11-21 22:12:55 +0300

    completed assingment

---
+++ index.js
@@ -0,0 +1,30 @@
+function add (a=3,b=5){
+    return a+b    
+}
+add();
+function subtract (a=6,b=4){
+    return a-b
+}
+subtract();
+function multiply(a=4,b=8){
+    return a*b
+}
+multiply();
+function divide (a=7,b=3){
+    return a/b
+}
+divide();
+
+function increment(n=""){
+    return (n+=1)
+}
+function decrement(n=""){
+    return(n-=1)
+}
+function makeInt(n){
+    return parseFloat(n)*1.0*1
+}
+function preserveDecimal(n){
+    return parseFloat(n)*1.0*1
+ 
+}
